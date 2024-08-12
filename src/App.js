151fe46edc7419d2f1dfa841f7a7e99c5a319049
Hiển Phan <87771924+pmh3107@users.noreply.github.com>
2024-08-12 16:31:04 +0700

    FIX: Add route in APP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LogInPage from "./pages/sideBar/sideBar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Hello world!
-      </h1>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LogInPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
