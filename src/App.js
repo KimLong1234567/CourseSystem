b211d131d43b95cc098bcd8f1a927ffaf793c0d9
Hiển Phan <87771924+pmh3107@users.noreply.github.com>
2024-08-13 13:54:00 +0700

    FEAT: Completed LogInPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LogInPage from "./pages/sideBar/sideBar";
-
+import LogInPage from "./pages/LogInPage/LogInPage";
+import HomePage from "./pages/HomePage/HomePage";
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<LogInPage />} />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/LogIn" element={<LogInPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
