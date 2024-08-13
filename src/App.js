2fbe7cbda32f2d980082229325cf94f4a34b5768
Kimlong1234567 <kimlonglam100@gmail.com>
2024-08-13 14:40:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LogInPage from "./pages/sideBar/sideBar";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LogInPage from './pages/sideBar/sideBar';
+import MainContent from './pages/mainContent/mainContent';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LogInPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<MainContent />} />
+        <Route path="/" element={<LogInPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
