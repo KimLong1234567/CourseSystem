b15a64692cb62c896f269fff6d729b31d1371974
Kimlong1234567 <kimlonglam100@gmail.com>
2024-08-14 00:24:42 +0700

    layout: sidebar

--- src/pages/sideBar/sideBarAdmin.jsx
+++ src/pages/sideBar/sideBarAdmin.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+// admin controller
 const ADMIN_SIDEBAR = [{ icon: faChartLine, name: 'Home', to: '/' }];
+
 function Sidebar() {
   return (
     <div className="">
       <h3 className="">This is Side bar</h3>
+      {ADMIN_SIDEBAR.map((item, index) => {
+        return (
+          <div key={index} className="flex justify-center border-zinc-900">
+            <Link to={item.to} className="name menuItem">
+              <Icon icon={item.icon} className="iconItem" />
+              <span>{item.name}</span>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
