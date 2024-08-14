558c167fc1774c279019be59edf3e54b492d5bfe
Hiển Phan <87771924+pmh3107@users.noreply.github.com>
2024-08-14 17:16:18 +0700

    FIX: fix app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import LogInPage from "./pages/LogInPage/LogInPage";
 import MainContent from "./pages/mainContent/mainContent";
 import ErrorPage from "./pages/error/ErrorPage";
+import CoursesPage from "./pages/coursesPage/CoursesPage";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route path="/admin" element={<MainContent />} />
 				<Route path="/" element={<HomePage />} />
+				<Route path="/CoursesPage" element={<CoursesPage />} />
 				<Route path="/LogIn" element={<LogInPage />} />
 				{/* Error page */}
 				<Route path="*" element={<ErrorPage />} />
