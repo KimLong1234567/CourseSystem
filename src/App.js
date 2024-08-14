617f17bb3c2522675a8a2e605a653aa554baa6b6
Kimlong1234567 <kimlonglam100@gmail.com>
2024-08-14 17:18:41 +0700

    layout: link

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import HomePage from './pages/HomePage/HomePage';
 import LogInPage from './pages/LogInPage/LogInPage';
 import MainContent from './pages/mainContent/mainContent';
 import ErrorPage from './pages/error/ErrorPage';
+import AdminCategory from './pages/Layout/AdminCategory/adminCategory';
+import AdminContent from './pages/Layout/AdminStudent/adminStudent';
+import AdminCompany from './pages/Layout/AdminCompany/adminCompany';
+import AdminCourses from './pages/Layout/AdminCourses/adminCourses';
 
 function App() {
   return (
     <BrowserRouter>
+      <Routes>
+        <Route element={<MainContent />}>
+          <Route path="/admin/student" element={<AdminContent />} />
+          <Route path="/admin/category" element={<AdminCategory />} />
+          <Route path="/admin/company" element={<AdminCompany />} />
+          <Route path="/admin/courses" element={<AdminCourses />} />
+        </Route>
+      </Routes>
       <Routes>
         <Route path="/admin" element={<MainContent />} />
         <Route path="/" element={<HomePage />} />
