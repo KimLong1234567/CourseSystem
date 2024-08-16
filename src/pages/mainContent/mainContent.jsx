ca0bcf431228a6cd137ebc645b4fdfb240d7b0d8
Kimlong1234567 <kimlonglam100@gmail.com>
2024-08-16 11:28:39 +0700

    feat: complete student

--- src/pages/mainContent/mainContent.jsx
+++ src/pages/mainContent/mainContent.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Sidebar from '../sideBar/sideBarAdmin';
 import HeaderAdmin from '../Layout/HeaderAdmin/header';
-import Profile from '../Layout/Profile/profile';
 import { Outlet } from 'react-router-dom';
 import './mainContent.css';
 
@@ -13,8 +12,8 @@ function MainContent() {
         gridTemplateRows: 'auto 2fr 20%',
         gridTemplateColumns: 'minmax(auto, 0.5fr) 3fr minmax(auto, 1fr)',
         gridTemplateAreas: `
-          "sidebar page header"
-          "sidebar page detail"
+          "sidebar header header"
+          "sidebar page page"
         `,
       }}
     >
@@ -36,9 +35,9 @@ function MainContent() {
       >
         <Outlet />
       </div>
-      <div style={{ gridArea: 'detail' }}>
+      {/* <div style={{ gridArea: 'detail' }}>
         <Profile />
-      </div>
+      </div> */}
     </div>
   );
 }
