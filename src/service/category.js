1d4bd33b7f59712800c9b6967e1434829ef87cef
Kimlong1234567 <kimlonglam100@gmail.com>
2024-08-21 08:59:21 +0700

    choke: config api

--- src/service/category.js
+++ src/service/category.js
@@ -8,7 +8,7 @@ const API_URL =
 export const getCategory = async () => {
   try {
     const response = await axios.get(API_URL);
-    return response.data;
+    return response.data.content;
   } catch (error) {
     console.error('Error fetching posts:', error);
     throw error;
