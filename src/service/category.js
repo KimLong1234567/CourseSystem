89afddf685910a52c17ab1132fa6a0de1a3b9567
Kimlong1234567 <kimlonglam100@gmail.com>
2024-08-21 09:06:54 +0700

    choke: resovle config

--- src/service/category.js
+++ src/service/category.js
@@ -8,7 +8,7 @@ const API_URL =
 export const getCategory = async () => {
   try {
     const response = await axios.get(API_URL);
-    return response.data.content;
+    return response.data;
   } catch (error) {
     console.error('Error fetching posts:', error);
     throw error;
