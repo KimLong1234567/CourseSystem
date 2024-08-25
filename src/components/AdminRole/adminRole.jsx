e23a6953769b68a631b98b5d75bb5b00e67dd4e3
Kimlong1234567 <kimlonglam100@gmail.com>
2024-08-25 12:22:06 +0700

    feat: private router

--- src/components/AdminRole/adminRole.jsx
+++ src/components/AdminRole/adminRole.jsx
@@ -1,9 +1,133 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Modal, Table } from 'antd';
+import { Form, Input } from 'antd';
+import {
+  getAllRole,
+  createRole,
+  updateRole,
+  deleteRole,
+} from '../../service/role';
+import Profile from '../Profile/coursesDetail';
 
 function AdminRole() {
+  const [form] = Form.useForm();
+  const [refresh, setRefresh] = useState(0);
   const [currentRecord, setCurrentRecord] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const [isProfileVisible, setIsProfileVisible] = useState(false);
+  const roleId = '73957c9a-0454-4d65-b683-287f2e72f608';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const roles = await getAllRole();
+        const rolesWithId = roles.map((roles, index) => ({
+          ...roles,
+          num: index + 1,
+        }));
+        setData(rolesWithId);
+      } catch (error) {
+        console.error('Error fetching category:', error);
+      }
+    };
+    fetchData();
+  }, [refresh]);
+
+  const handleDetail = (record) => {
+    setCurrentRecord(record);
+    // setIsProfileVisible(true);
+  };
+
+  const handleUpdate = (record) => {
+    setCurrentRecord(record);
+    setIsModalOpen(true);
+  };
+
+  const columns = [
+    {
+      title: 'Id',
+      dataIndex: 'num',
+      sorter: (a, b) => a.num - b.num,
+      width: '5%',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      width: '15%',
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      render: (text, record) => (
+        <span>
+          <Button
+            className="ml-3 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => handleDetail(record)}
+          >
+            Detail
+          </Button>
+          <Button
+            className="bg-yellow-400 text-white"
+            onClick={() => handleUpdate(record)}
+          >
+            Update
+          </Button>
+          <Button
+            className="bg-red-700 text-white"
+            onClick={() => handleDelete(record)}
+          >
+            Delete
+          </Button>
+        </span>
+      ),
+    },
+  ];
+
+  const handleDelete = async (record) => {
+    try {
+      await deleteRole(record.id);
+      setRefresh((prev) => prev + 1);
+    } catch (error) {
+      console.error('Error deleting student:', error);
+    }
+  };
+
+  const handleFormSubmit = async (values) => {
+    console.log(values);
+    try {
+      const data = {
+        ...values, // Spread the values object to include its properties directly
+        permissions: [
+          {
+            id: roleId,
+          },
+        ],
+      };
+
+      console.log('Submitted Data:', data); // This will log the correctly structured data
+
+      if (currentRecord) {
+        await updateRole(currentRecord.id, data);
+        setIsModalOpen(false);
+        form.resetFields();
+      } else {
+        await createRole(data);
+        setIsModalOpen(false);
+        form.resetFields();
+      }
+      setRefresh((prev) => prev + 1);
+      setIsModalOpen(false);
+    } catch (error) {
+      console.error('Error saving account:', error);
+    }
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    form.resetFields();
+    setCurrentRecord(null);
+  };
 
   return (
     <div>
@@ -18,8 +142,69 @@ function AdminRole() {
         }}
         className="bg-blue-500 text-white"
       >
-        Add Category
+        Add Role
       </Button>
+      <Modal
+        title={currentRecord ? 'Update Role' : 'Add Role'}
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <Form
+          form={form}
+          name="role"
+          onFinish={handleFormSubmit}
+          layout="vertical"
+          initialValues={{
+            currentRecord,
+          }}
+        >
+          <Form.Item
+            label="Role name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: 'Please input the role name!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              {currentRecord ? 'Update Role' : 'Add Role'}
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <div
+        className="grid"
+        style={{
+          gridTemplateRows: 'auto',
+          gridTemplateColumns: '3fr minmax(auto, 1fr)',
+          gridTemplateAreas: `
+                "table profile"
+              `,
+        }}
+      >
+        <Table
+          style={{ gridArea: 'table' }}
+          className="mt-5"
+          columns={columns}
+          dataSource={data}
+          rowKey="id"
+        />
+        <div className="relative mt-5 ml-4" style={{ gridArea: 'profile' }}>
+          {isProfileVisible && currentRecord && (
+            <Profile
+              {...currentRecord}
+              onClose={() => setIsProfileVisible(false)}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/service/role.js
@@ -0,0 +1,85 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const API_URL = 'http://192.168.1.25:8080/api/roles';
+// Service để gọi các API liên quan đến Post
+export const createRole = async (role) => {
+  console.log('Role Data Sent to Backend:', role);
+  try {
+    const response = await axios.post(API_URL, role).then((res) => {
+      toast.success('Create success', {
+        position: 'top-center',
+        autoClose: 2000,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+    });
+    return response?.data;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    throw error;
+  }
+};
+
+export const getRoleById = async (id) => {
+  try {
+    const response = await axios.get(`${API_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching post:', error);
+    throw error;
+  }
+};
+
+export const getAllRole = async (post) => {
+  try {
+    const response = await axios.get(API_URL, post);
+    return response.data.content;
+  } catch (error) {
+    console.error('Error creating post:', error);
+    throw error;
+  }
+};
+
+export const updateRole = async (id, post) => {
+  try {
+    console.log(id, post);
+    const response = await axios.put(`${API_URL}/${id}`, post).then((res) => {
+      toast.info('Update success', {
+        position: 'top-center',
+        autoClose: 2000,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+    });
+    return response;
+  } catch (error) {
+    console.error('Error updating post:', error);
+    throw error;
+  }
+};
+
+export const deleteRole = async (id) => {
+  try {
+    await axios.delete(`${API_URL}/${id}`).then((res) => {
+      toast.error('Delete success', {
+        position: 'top-center',
+        autoClose: 2000,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+    });
+  } catch (error) {
+    console.error('Error deleting post:', error);
+    throw error;
+  }
+};
